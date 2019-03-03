3cf144247b36efd87d56a381e3b62a0fe3f76a8f
leonshen95 <leyang.shen@uconn.edu>
2019-03-03 02:01:09 -0500

    Update test_duration.py

--- test_duration.py
+++ test_duration.py
@@ -4,14 +4,14 @@ from pytest import approx
 
 def test_duration():
     fnin = 'realshort.mp4'
-    fnout = 'realshort480.mp4'
+    fnout = 'realshort480_test.mp4'
 
     orig_meta = probe.ffprobe_sync(fnin)
     orig_duration = float(orig_meta['streams'][0]['duration'])
 
-    convert(fnin, fnout, 480)
+    convert.main()
 
-    meta_480 = probe.ffprobe(fnout)
+    meta_480 = probe.ffprobe_sync(fnout)
     duration_480 = float(meta_480['streams'][0]['duration'])
 
     assert orig_duration == approx(duration_480)
