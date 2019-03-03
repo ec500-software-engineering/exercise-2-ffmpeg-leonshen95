1bf43f06b25d6a16aa66bff49e445dfab30a02d1
leonshen95 <leyang.shen@uconn.edu>
2019-03-03 01:51:23 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='ex2',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Leyang Shen',
+    author_email='leonshen@bu.edu',
+    install_requires=["flake8", "pytest"],
+    description='Use thread and coroutine to convert video'
+)
