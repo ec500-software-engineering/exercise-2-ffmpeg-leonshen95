b9d45598e907704214d03dfb57a8012d7b3858f4
leonshen95 <leyang.shen@uconn.edu>
2019-03-06 15:14:08 -0500

    Update test_duration.py

--- test_duration.py
+++ test_duration.py
@@ -3,8 +3,8 @@ import subprocess
 
 
 def test_duration():
-    inp='2mb.avi'
-    out='2.avi'
+    inp='realshort.mp4'
+    out='realshort_480.mp4'
     outLen = subprocess.call(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',out])
     inLen  = subprocess.call(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',inp])
     assert inLen == approx(outLen)
